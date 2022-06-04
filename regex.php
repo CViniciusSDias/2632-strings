0f413cd7ee351e2e5c125b9a9abbe505991647a3
Vinicius Dias <carlosv775@gmail.com>
2022-06-04 17:52:20 -0300

    Video 5.3

--- regex.php
+++ regex.php
@@ -1,18 +1,24 @@
 <?php
 
-$telefones = ['algo (24) 99999 - 9999', '(21) 99999 - 9999 coisa', '(24) 2222 - 2222'];
+$telefones = ['(24) 99999 - 9999', '(21) 99999 - 9999', '(24) 2222 - 2222'];
 
 foreach ($telefones as $telefone) {
+    $regex = '/^\(([0-9]{2})\) (9?[0-9]{4} - [0-9]{4})$/';
     $telefoneValido = preg_match(
-        '/(\([0-9]{2}\)) 9?[0-9]{4} - [0-9]{4}/',
+        $regex,
         $telefone,
         $correspondencia
     );
 
-    var_dump($correspondencia);
     if ($telefoneValido) {
         echo 'Telefone válido'. PHP_EOL;
     } else {
         echo 'Telefone inválido'. PHP_EOL;
     }
+
+    echo preg_replace(
+        $regex,
+        '(XX) \2',
+        $telefone
+    ) . PHP_EOL;
 }
