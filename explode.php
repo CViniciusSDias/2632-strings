61db70e0aea952ac3e8744bfcce2424084daca78
Vinicius Dias <carlosv775@gmail.com>
2022-06-04 15:27:37 -0300

    Video 3.2

---
+++ explode.php
@@ -0,0 +1,5 @@
+<?php
+
+$array = '1 2 3 4';
+
+var_dump(explode(' ', $array, 2));
---
+++ telefones.php
@@ -0,0 +1,5 @@
+<?php
+
+$telefones = ['(24) 99999 - 9999 ', ' (21) 99999 - 9999', '(24) 2222 - 2222'];
+
+echo implode(array: $telefones, separator: ', ') . PHP_EOL;
