7e8006d69bcb54765af72d1024d6d120ff30ea2c
Vinicius Dias <carlosv775@gmail.com>
2022-06-04 15:49:11 -0300

    Video 4.1

---
+++ numeros.php
@@ -0,0 +1,13 @@
+<?php
+
+$anoNascimento = '1997';
+
+$idade = 2022 - $anoNascimento;
+
+echo $idade . PHP_EOL;
+
+if ($anoNascimento == 1997) {
+    echo 'SIM' . PHP_EOL;
+} else {
+    echo 'NÃO' . PHP_EOL;
+}
